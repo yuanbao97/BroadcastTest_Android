4335d46113c793a7f0631782c011d0f07b12922a
yuanbao97 <316261393@qq.com>
2021-01-22 13:35:36 +0800

    5.3.1 发送自定义标准广播

--- app/src/main/java/com/example/broadcasttest/activity/MainActivity.java
+++ app/src/main/java/com/example/broadcasttest/activity/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.broadcasttest.activity;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.broadcasttest.R;
 import com.example.broadcasttest.receiver.NetworkChangeReceiver;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.sendBroadcastButton)
+    Button sendBroadcastButton;
     private IntentFilter intentFilter;
     private NetworkChangeReceiver networkChangeReceiver;
 
@@ -17,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         intentFilter = new IntentFilter();
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
@@ -30,4 +41,11 @@ public class MainActivity extends AppCompatActivity {
         unregisterReceiver(networkChangeReceiver);
     }
 
+    @OnClick(R.id.sendBroadcastButton)
+    public void onClick() {
+        Intent intent = new Intent("com.example.broadcasttest.receiver.MY_BROADCAST");
+        intent.setComponent(new ComponentName(getPackageName(), "com.example.broadcasttest.receiver.MyBroadcastReceiver"));
+        intent.putExtra("myBroadcast", "hello world !");
+        sendBroadcast(intent);
+    }
 }
